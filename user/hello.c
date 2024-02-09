7bc2e2f22f44d2a96d763e5ba95d27343e722618
Christian Fredrik Johnsen <christian@johnsen.no>
2024-02-09 02:29:27 +0100

    Passes the hello world test.

--- user/hello.c
+++ user/hello.c
@@ -2,9 +2,16 @@
 #include "user/user.h"
 #include "kernel/stat.h"
 
+/*
+Make clean to remove all of the compiled stuff.
+*/
 
+int main(int argc, char *argv[]) { // The standard main declaration
 
-int main(void) {
-    printf("Hello World!\n");
-    exit(0);
+    if (argc > 1) { // argc is always at least 1, since the program name counts as an argument
+        printf("Hello %s, nice to meet you!\n", argv[1]);
+    } else {
+        printf("Hello World\n");
+    }
+    return 0; // 0 usually means success
 }
