0a51d24130add38a895a97b3e40985eded0c1de0
Christian Fredrik Johnsen <christian@johnsen.no>
2024-03-13 15:42:13 +0100

    test: add folder for testing purposes.

---
+++ c_testing/array.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int array[] = {1, 2, 3, 4, 5};
+    int length = sizeof(array) / sizeof(array[0]);
+
+    for (int i = 0; i < length; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+
+    int *point;
+
+    for (int *p = array; p < &array[length]; p++) {
+        printf("Digit: %d ", *p);
+        if (*p == 5) {
+            printf("Last digit\n");
+            point = p;
+        }
+        else {
+            printf("Not last digit\n");
+        }
+        printf("Address: %p\n", p);
+    }
+
+    printf("Array address: %p\n", array);
+    printf("Integer value: %d\n", *point);
+
+
+    return 0;
+}
