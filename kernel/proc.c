80affee6e1f8f203d4356e241f7a06cd6316a751
Christian Fredrik Johnsen <christian@johnsen.no>
2024-03-19 11:38:22 +0100

    style: initial comments and code-restructuring in fork-method in proc.c

--- kernel/proc.c
+++ kernel/proc.c
@@ -373,43 +373,33 @@ int growproc(int n)
 
 // Create a new process, copying the parent.
 // Sets up child kernel stack to return as if from fork() system call.
-int fork(void)
-{
-    int i, pid;
+int fork(void) {
     struct proc *np;
     struct proc *p = myproc();
 
     // Allocate process.
-    if ((np = allocproc()) == 0)
-    {
+    if ((np = allocproc()) == 0) {
         return -1;
     }
 
     // Copy user memory from parent to child.
-    if (uvmcopy(p->pagetable, np->pagetable, p->sz) < 0)
-    {
+    if (uvmcopy(p->pagetable, np->pagetable, p->sz) < 0) {
         freeproc(np);
         release(&np->lock);
         return -1;
     }
+    
     np->sz = p->sz;
+    *(np->trapframe) = *(p->trapframe); // copy saved user registers.
+    np->trapframe->a0 = 0; // Cause fork to return 0 in the child.
 
-    // copy saved user registers.
-    *(np->trapframe) = *(p->trapframe);
-
-    // Cause fork to return 0 in the child.
-    np->trapframe->a0 = 0;
-
-    // increment reference counts on open file descriptors.
-    for (i = 0; i < NOFILE; i++)
+    for (int i = 0; i < NOFILE; i++) // increment reference counts on open file descriptors.
         if (p->ofile[i])
             np->ofile[i] = filedup(p->ofile[i]);
-    np->cwd = idup(p->cwd);
+    np->cwd = idup(p->cwd); // No idea
 
     safestrcpy(np->name, p->name, sizeof(p->name));
-
-    pid = np->pid;
-
+    int pid = np->pid;
     release(&np->lock);
 
     acquire(&wait_lock);
